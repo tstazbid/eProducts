96f082df8e0add1b56fa3c50af257e7fdedb62a9
tstazbid <tazbidus15-13711@diu.edu.bd>
2024-06-13 13:40:07 +0600

    model build

---
+++ myshop/products/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    subcategory = models.ForeignKey(SubCategory, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    view_count = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+    
+
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.name}"
